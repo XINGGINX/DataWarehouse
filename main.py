eed62c542cb28b857df94fb4795663fab5f40014
XINGGINX <100259620+XINGGINX@users.noreply.github.com>
2022-03-03 03:15:05 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-import os 
+# Module import 
+import getpass
 
-
-
-
+# helper files
+import controller
 import helper
-import land
-import extract
-import transfer
-import load
+
+# section boarder 
+section_start_log = "-" * 90
+section_finish_log = section_start_log + "\n"
+
+#=========================================================
+#                   Main function
+#=========================================================
+if __name__ == "__main__":
+    print(section_start_log)
+
+    conf = helper.read_config("./config/conf.json")
+
+    # asking password for 3 times
+    for tolerance in range(0, 3):
+        print("- main(): ENTER PASSWORD ...")
+
+        # asking for password and verify 
+        password = getpass.getpass()
+        unlock = helper.create_connection(password, conf, log = False)
+
+        # Stop early if password is correct
+        if (unlock != False):
+            break
+        else:
+            print("- main(): PASSWORD INCORRECT ... \n")
+
+
+    # using controller to get command 
+    if(unlock != False):
+        print(section_start_log)
+        print("- main(): STARTING ... \n\n")
+        controller.controller(password)
+    else:
+        print("- main(): MAX ATTEMPT EXCEEDED ...")
+        print(section_finish_log)
+        print(40*"\n")
+        exit(0)
